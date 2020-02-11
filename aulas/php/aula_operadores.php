b7cc30d6512217ca7e4b811011e76e625cc68e89
Vilmar Nunes <vilmarmiranda@gmail.com>
2020-02-11 20:32:12 -0200

    aula de operadores

---
+++ aulas/php/aula_operadores.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>aula de operadores</title>
+</head>
+<body>
+	<?php
+		$num1=1;
+		$num2=2;
+
+		$soma=$num1+$num2;
+
+		echo "A soma dos numeros é $soma";
+	?>
+</body>
+</html>
--- aulas/php/funcao2.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	include "aula1.php";
-
-	$resultado=	somar(10, 20);
-
-	echo "$resultado";
-?>
