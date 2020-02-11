d17650e0dc290363ff410aae5ac732b55a2d4be5
Vilmar Nunes <vilmarmiranda@gmail.com>
2020-02-11 21:37:01 -0200

    Aula de operadores em php

--- aulas/php/aula_operadores.php
+++ aulas/php/aula_operadores.php
@@ -6,12 +6,19 @@
 </head>
 <body>
 	<?php
-		$num1=1;
-		$num2=2;
+		//valores passados por parametro (?a=10&b=2)
+		$num1 =$_GET["a"];
+		$num2= $_GET["b"];
 
 		$soma=$num1+$num2;
 
-		echo "A soma dos numeros é $soma";
+		echo "A soma dos numeros é $soma <br>";
+		echo "o valor absoluto da variavel é ".abs($num2)."<br>";
+		echo "O valor de $num1 <sup>$num2</sup> é ". pow($num1, $num2);
+		echo "<br>A raiz quadrada de $num1 é ". sqrt($num1);
+		echo "<br> O valor de $num1 arredondado é ". round($num1);
+		echo "<br> A parte inteira de $num2 é ". intval($num2);
 	?>
 </body>
 </html>
+
